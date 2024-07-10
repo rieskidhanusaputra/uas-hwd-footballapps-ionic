64e1043ecd8ca93f05fc602728bbdcc48ee1e6de
rieskidhanusaputra <rieskidhanu17@gmail.com>
2024-07-10 14:38:30 +0700

    first commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MATCH} from '../../assets/data';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  public match = MATCH;
   constructor() {}
 
 }
